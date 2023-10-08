585809fb285452f5c47fc4efa3459498354a5d0a
DIego <mendesdiego90@gmail.com>
2023-10-09 01:33:56 +0900

    now that there is code to make sure the email is unique, we need to tell the user that email is already taken Prisma gives error message in that case. So, the prisma error should be catched and, check if this error is an instance of prisma. Then grab the error.code and throw a Nestjs ForbiddenException, which receives a string as argument that will be the error message

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,8 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { User, Bookmark } from '@prisma/client'
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto';
 import * as argon from 'argon2';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library';
 //prisma generate the typescript types automatically
 
 //the Injectable decorator (for service) and the class declaration
@@ -21,6 +22,7 @@ export class AuthService {
       const hash = await argon.hash(dto.password) //using the installed package to generate a hash of the password
       //saving the new user in the db
 
+      try {
       const user = await this.prisma.user.create({
          //the using the create to create a row in the user table using prisma
          data: {
@@ -34,7 +36,17 @@ export class AuthService {
       delete user.hash //this is a temporary solution to remove the hash, 
       //because the select approach is verbose
       //return the saved user
-   return user;
+      return user;
+   } catch(err) {
+      if (err instanceof PrismaClientKnownRequestError) {
+         if (err.code === 'P2002') {
+            //the err.code = P2002 is the specific code for duplicate error in prisma
+            throw new ForbiddenException('Credentials taken')
+         }
+         throw err
+      }
+   }
+   
    //the user object doesn't have the hash password because it was not included in the select statement
    return 'signed up'
    }
