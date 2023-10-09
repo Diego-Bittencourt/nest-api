8c8026bd6a05862f58a04d302734731a5d1540c1
DIego <mendesdiego90@gmail.com>
2023-10-09 23:44:22 +0900

    installed the ConfigModule from nestjs
    the config module can be used inside a custom module or as a global module,
    as I am doing here.
    I am adding the ConfigModule in the root app module and using the isGlobal
    to make it global throughout the app
    Then, I added it to the constructor in the prisma class and use it to get
    the db link from the .env file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,26 @@
-import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { BookmarkModule } from './bookmark/bookmark.module';
-import { PrismaModule } from './prisma/prisma.module';
+import { Module } from "@nestjs/common";
+import { AuthModule } from "./auth/auth.module";
+import { UserModule } from "./user/user.module";
+import { BookmarkModule } from "./bookmark/bookmark.module";
+import { PrismaModule } from "./prisma/prisma.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }), //this module is being set to be global and it allows to config the env dev
+    //under the hood, the module above uses dotenv library
+    AuthModule, 
+    UserModule, 
+    BookmarkModule, 
+    PrismaModule
+  ],
 })
 export class AppModule {}
 
 //decorator is a function that submis metadata to other functions
 // module can import other modules and this file is the main module in the application
 //the good practice is to each module be on its module, except the main module which is this one
-
 
 //importing the AuthModule
