c9e6a0ca6d83455f6d4ed558283bc135b569b1ed
DIego <mendesdiego90@gmail.com>
2023-10-12 00:29:59 +0900

    added new functionality to the @GetUser custom decorator
    added the possibility to add a string to the @GetUser decorator argument
    and return only that field in the user object

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,23 +1,20 @@
 import { Controller, Get, UseGuards, Req } from "@nestjs/common";
 import { AuthGuard } from "@nestjs/passport";
 import { JwtGuard } from '../auth/guard/index'
-import { Request } from "express";
 import { UserService } from "./user.service";
 import { GetUser } from "src/auth/decorator/get-user.decorator";
 import { User } from "@prisma/client";
 
+@UseGuards(JwtGuard) //Using the guards at top level, affecting all endpoints in here
 @Controller('user')
 export class UserController {
     constructor(
         private userService: UserService
     ) {}
 
-    @UseGuards(JwtGuard) //this line tell this endpoint is protected byt 'jwt' strategy
     @Get('me')
     getMe(@GetUser() user: User) {
         //note that the Request type must be from express, so the req.user doesn't give error
-        console.log(user) 
-        //the console.log(req.user) consoles the same as the payload in the validate function
         return user
     }
 }
