95d861402a768d70d64e28c3a5eb147971402ec4
DIego <mendesdiego90@gmail.com>
2023-10-15 23:31:46 +0900

    created end2end testing for the signup and login endpoints
    For testing, Im using Jest and pactum.
    Pactum allows me to create it() functions that receive one message
    and one arrow function which is going to perform the testing.
    The pactum has method to create all sort of requests and expect
    result from the endpoint, so I can create many different test cases
    and they are going to run and check is the app is working end to end.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -3,6 +3,8 @@ import { AppModule } from "../src/app.module";
 import { INestApplication, ValidationPipe } from "@nestjs/common";
 import { after } from "node:test";
 import { PrismaService } from "../src/prisma/prisma.service";
+import * as pactum from "pactum";
+import { AuthDto } from "../src/auth/dto";
 
 describe("App e2e", () => {
   let app: INestApplication;
@@ -20,42 +22,110 @@ describe("App e2e", () => {
       })
     );
     await app.init();
+    await app.listen(3333);
 
     //reseting the database
     prisma = app.get(PrismaService);
     await prisma.cleanDb();
+    pactum.request.setBaseUrl("http://localhost:3333/");
   });
-
 
   afterAll(() => {
     app.close();
   });
 
-  describe('Auth', () => {
-    describe('Signup', () => {
-      it.todo('should sign up')
-    })
+  describe("Auth", () => {
+    const dto: AuthDto = {
+      email: "didi@email.com",
+      password: "a1s2d3",
+    };
+    describe("Signup", () => {
 
-    describe('Login', () => {
-      it.todo('logged in')
-    })
-  })
+      it('should throw error email empty', () => {
+        return pactum
+        .spec()
+        .post('auth/signup')
+        .withBody({ password: dto.password})
+        .expectStatus(400);
+      })
 
-  describe('User', () => {
-    describe('Get me', () => {});
+      it('should throw error password empty', () => {
+        return pactum
+        .spec()
+        .post('auth/signup')
+        .withBody({email: dto.email})
+        .expectStatus(400)
+      })
 
-    describe('Edit user', () => {});
-  })
+      it('should complain about no body', () => {
+        return pactum
+        .spec()
+        .post('auth/signup')
+        .expectStatus(400)
+      })
 
-  describe('Bookmark', () => {
-    describe('Create bookmark', () => {});
 
-    describe('Get bookmark', () => {});
+      it("should sign up", () => {
+        return pactum
+          .spec()
+          .post("auth/signup")
+          .withBody(dto)
+          .expectStatus(201)
+          // .inspect(); //this one logs the body in the console as warning
+      });
+    });
 
-    describe('Get bookmark by id', () => {});
+    describe("Login", () => {
 
-    describe('Edit bookmark', () => {});
+      it('should throw error email empty', () => {
+        return pactum
+        .spec()
+        .post('auth/login')
+        .withBody({ password: dto.password})
+        .expectStatus(400);
+      })
 
-    describe('Delete bookmark', () => {});
-  })
+      it('should throw error password empty', () => {
+        return pactum
+        .spec()
+        .post('auth/login')
+        .withBody({email: dto.email})
+        .expectStatus(400)
+      })
+
+      it('should complain about no body', () => {
+        return pactum
+        .spec()
+        .post('auth/login')
+        .expectStatus(400)
+      })
+
+
+      it("logged in", () => {
+        return pactum
+          .spec()
+          .post("auth/login")
+          .withBody(dto)
+          .expectStatus(200)
+      });
+    });
+  });
+
+  describe("User", () => {
+    describe("Get me", () => {});
+
+    describe("Edit user", () => {}); 
+  });
+
+  describe("Bookmark", () => {
+    describe("Create bookmark", () => {});
+
+    describe("Get bookmark", () => {});
+
+    describe("Get bookmark by id", () => {});
+
+    describe("Edit bookmark", () => {});
+
+    describe("Delete bookmark", () => {});
+  });
 });
